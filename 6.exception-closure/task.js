1fbe05207ed92c1683300bfdff6a67252c547a0d
shamagir <shamagir@gmail.com>
2021-07-15 14:28:59 +0300

    fix: bjs-2-homeworks 6.exception & closure refactoring

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -19,51 +19,37 @@ function validateCount(argument) {
 // Задача №2
 
 class Triangle {
-
-	#p;
-
 	constructor(a, b, c) {
+		if(isNaN(a) || isNaN(b) && isNaN(c) || a + b < c || a + c < b || b + c < a) {
+			throw new Error('Треугольник с такими сторонами не существует');
+		}
 		this.a = a;
 		this.b = b;
 		this.c = c;
-		this.perimetr = this.getPerimeter();
-		this.area = this.getArea();
-		this.checkTriangle();
 	}
 
 	getPerimeter() {
-			return this.perimetr = this.a + this.b +this.c;
+		return this.a + this.b +this.c;
 	}
 
 	getArea() {
-		this.#p = this.perimetr / 2;
-		this.area = Math.sqrt(this.#p * (this.#p - this.a) * (this.#p - this.b) * (this.#p - this.c));
+		let p = this.getPerimeter() / 2;
+		this.area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
 		this.area = Number(this.area.toFixed(3));
-		
 		return this.area
-	}
-
-	checkTriangle() {
-		if(isNaN(this.a) || isNaN(this.b) && isNaN(this.c) || this.a + this.b < this.c || this.a + this.c < this.b || this.b + this.c < this.a) {
-			throw new Error('Треугольник с такими сторонами не существует');
-		}
-		return;
 	}
 }
 
 function getTriangle(a, b, c) {
 	try{
-		return triangle =  new Triangle(a, b, c);
-	} catch {
-		class pseudoTriangele {
-			getPerimeter() {
-				return this.perimetr = 'Ошибка! Треугольник не существует';
-			}
-
-			getArea() {
-				return this.area = 'Ошибка! Треугольник не существует';
-			}
+		return new Triangle(a, b, c);
+	} catch(error) {
+		return {
+			getPerimeter: () => 'Ошибка! Треугольник не существует',
+			getArea:      () => 'Ошибка! Треугольник не существует'
 		}
-		return triangle =  new pseudoTriangele();
 	}
 }
+
+triangle =  getTriangle(1, 3, 100);
+console.log(triangle.getPerimeter());
